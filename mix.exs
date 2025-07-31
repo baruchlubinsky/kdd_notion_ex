2c60d84efd2b3bffc77945e1518d340bff19063c
Baruch Lubinsky <developer@karmadrivendevelopment.org>
2025-07-31 15:45:46 +0200

    version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule KddNotionEx.MixProject do
   def project do
     [
       app: :kdd_notion_ex,
-      version: "0.2.0",
+      version: "0.2.1",
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
       deps: deps()
