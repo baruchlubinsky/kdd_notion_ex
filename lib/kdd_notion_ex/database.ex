8f0a2d9bb5738d5288db139ee53d9249cbfcaf98
Baruch Lubinsky <developer@karmadrivendevelopment.org>
2025-08-04 17:24:38 +0200

    fetch page content

--- lib/kdd_notion_ex/database.ex
+++ lib/kdd_notion_ex/database.ex
@@ -1,7 +1,7 @@
 defmodule KddNotionEx.Database do
 
   def get_properties(req, database_id) do
-    {_, data} = Cachex.fetch(:notion_databases, database_id, fn id ->
+    {_, data} = Cachex.fetch(KddNotionEx.Cache.properties(), database_id, fn id ->
       {
         :commit,
         Req.get!(req, url: "/databases/#{id}")
@@ -12,13 +12,33 @@ defmodule KddNotionEx.Database do
   end
 
   def query(req, database_id, query) do
+    {_, data} = Cachex.fetch(KddNotionEx.Cache.queries(), {database_id, query}, fn _ ->
+      {
+        :commit,
+        Req.post!(req, url: "/databases/#{database_id}/query", json: query)
+        |> case do
+          %Req.Response{status: 200, body: response} ->
+            if response["has_more"] do
+            query = query || %{}
+              |> Map.put("start_cursor", response["next_cursor"])
+            response["results"] ++ query_page(req, database_id, query)
+          else
+            response["results"]
+          end
+        end
+      }
+    end)
+    data
+  end
+
+  def query_page(req, database_id, query) do
     Req.post!(req, url: "/databases/#{database_id}/query", json: query)
     |> case do
       %Req.Response{status: 200, body: response} ->
         if response["has_more"] do
         query = query || %{}
           |> Map.put("start_cursor", response["next_cursor"])
-        response["results"] ++ query(req, database_id, query)
+        response["results"] ++ query_page(req, database_id, query)
       else
         response["results"]
       end
--- lib/kdd_notion_ex/page.ex
+++ lib/kdd_notion_ex/page.ex
@@ -5,7 +5,7 @@ defmodule KddNotionEx.Page do
   end
 
   def fetch(req, page_id) do
-    {_, data} = Cachex.fetch(:notion_pages, page_id, fn id ->
+    {_, data} = Cachex.fetch(KddNotionEx.Cache.pages(), page_id, fn id ->
       {
         :commit,
         get(req, id)
@@ -24,14 +24,14 @@ defmodule KddNotionEx.Page do
 
     case response do
       %Req.Response{status: 200, body: body} ->
-        Cachex.put(:notion_pages, body["id"], body)
+        Cachex.put(KddNotionEx.Cache.pages(), body["id"], body)
     end
 
     response
   end
 
   def update(req, properties, page_id) do
-    Cachex.del(:notion_pages, page_id)
+    Cachex.del(KddNotionEx.Cache.pages(), page_id)
     payload = %{
       properties: properties
     }
@@ -39,19 +39,32 @@ defmodule KddNotionEx.Page do
 
     case response do
       %Req.Response{status: 200, body: body} ->
-        Cachex.put(:notion_pages, body["id"], body)
+        Cachex.put(KddNotionEx.Cache.pages(), body["id"], body)
     end
 
     response
   end
 
-  def fetch_content(req, page_id, start_cursor \\ nil) do
-    url = if is_nil(start_cursor) do
-      "/blocks/#{page_id}/children"
-    else
-      "/blocks/#{page_id}/children?start_cursor=#{start_cursor}"
-    end
-    Req.get!(req, url: url)
+  def fetch_content(req, page_id) do
+    {_, data} = Cachex.fetch(KddNotionEx.Cache.content(), page_id, fn id ->
+      {
+        :commit,
+        Req.get!(req, url: "/blocks/#{id}/children")
+        |> case do
+          %Req.Response{status: 200, body: response} ->
+            if response["has_more"] do
+              response["results"] ++ fetch_content(req, id, response["next_cursor"])
+            else
+              response["results"]
+            end
+        end
+      }
+    end)
+    data
+  end
+
+  def fetch_content(req, page_id, start_cursor) do
+    Req.get!(req, url: "/blocks/#{page_id}/children?start_cursor=#{start_cursor}")
     |> case do
       %Req.Response{status: 200, body: response} ->
         if response["has_more"] do
@@ -60,32 +73,55 @@ defmodule KddNotionEx.Page do
           response["results"]
         end
     end
+  end
 
-    def elements(blocks) do
-      Enum.reject(blocks, fn b -> b["archived"] end)
-      |> Enum.map(&block_as_elements/1)
+  def elements(blocks) do
+    Enum.reject(blocks, fn b -> b["archived"] end)
+    |> Enum.map(&block_as_elements/1)
+  end
+
+  def block_as_elements(block) when is_list(block) do
+    Enum.map(block, &block_as_elements/1)
+  end
+
+  def block_as_elements(block) when is_binary(block) do
+    block
+  end
+
+  def block_as_elements(%{"type" => "paragraph"} = block) do
+    {:p, block_as_elements(block["paragraph"]["rich_text"])}
+  end
+
+  def block_as_elements(%{"type" => "callout"} = block) do
+    {:p, block_as_elements(block["callout"]["rich_text"])}
+  end
+
+  def block_as_elements(%{"type" => "text"} = block) do
+    if is_nil(block["href"]) do
+      {:s, block_as_elements(block["text"]["content"])}
+    else
+      {:s, {:a, block["href"], block["text"]["content"]}}
     end
+  end
 
-    def block_as_elements(block) when is_list(block) do
-      Enum.map(block, &block_as_elements/1)
-    end
+  def block_as_elements(%{"type" => "heading_1"} = block) do
+    {:h1, block_as_elements(block["heading_1"]["rich_text"])}
+  end
 
-    def block_as_elements(%{"type" => "paragraph"} = block) do
-      {:p, block_as_elements(block["paragraph"]["rich_text"])}
-    end
+  def block_as_elements(%{"type" => "heading_2"} = block) do
+    {:h2, block_as_elements(block["heading_2"]["rich_text"])}
+  end
 
-    def block_as_elements(%{"type" => "text"} = block) do
-      if is_nil(block["href"]) do
-        {:s, block_as_elements(block["text"]["content"])}
-      else
-        {:s, {:a, block["href"], block["text"]["content"]}}
-      end
-    end
+  def block_as_elements(%{"type" => "heading_3"} = block) do
+    {:h3, block_as_elements(block["heading_3"]["rich_text"])}
+  end
 
-    def block_as_elements(%{"type" => "divider"}) do
-      {:hr}
-    end
+  def block_as_elements(%{"type" => "divider"}) do
+    {:hr}
+  end
 
+  def block_as_elements(%{"type" => "image", "image" => %{"file" => %{"url" => url}}}) do
+    {:image, url}
   end
 
 end
