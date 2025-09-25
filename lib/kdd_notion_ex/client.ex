643f2dd106713e9cfc9bd38c0475a0cac09c0e4b
Baruch Lubinsky <developer@karmadrivendevelopment.org>
2025-09-25 15:30:32 +0200

    bump version in header

--- lib/kdd_notion_ex/client.ex
+++ lib/kdd_notion_ex/client.ex
@@ -2,7 +2,7 @@ defmodule KddNotionEx.Client do
   def headers() do
     [
       {"Content-Type", "application/json"},
-      {"Notion-Version", "2022-06-28"}
+      {"Notion-Version", "2025‑09‑03"}
     ]
   end
 
