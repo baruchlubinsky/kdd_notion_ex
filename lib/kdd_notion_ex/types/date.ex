6302ffd3d65607ffbaec9e2a6614fca8ca36e291
Baruch Lubinsky <developer@karmadrivendevelopment.org>
2025-12-25 09:16:21 +0200

    timing

--- lib/kdd_notion_ex/types/date.ex
+++ lib/kdd_notion_ex/types/date.ex
@@ -6,6 +6,7 @@ defmodule KddNotionEx.Types.Date do
     case KddNotionEx.Transform.parse_property(param) do
       {start_time, _} -> {:ok, start_time}
       %{"date" => nil} -> {:ok, nil}
+      date -> {:ok, date}
     end 
   end
   
